f0105d9393aef7464a37839502182a6544cfe1e5
LuffySir <luffysir@mail.dlut.edu.cn>
2016-04-30 13:22:09 +0800

    Lee

---
+++ src/TwoSum.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Lenovo on 2016/4/10.
+ */
+public class TwoSum {
+    public int[] sum(int[] nums, int target){
+        int len = nums.length;
+        int i=0,j=0;
+
+        for(i=0;i<len;i++) {
+            for (j=i+1;j<len;j++){
+                if (nums[i]+nums[j]==target)
+                    System.out.println(i+"and"+j);
+            }
+        }
+        int[] res;
+        res = new int[]{i,j};
+        return res;
+    }
+    public static final void  main(String[] args){
+        sum();
+    }
+}
